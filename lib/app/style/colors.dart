e4846732e2191c9c548e3f568d75a18eaf4e1622
Dimas Salahuddin <dimassalah10@gmail.com>
2024-02-26 14:04:30 +0700

    feat: add design system

---
+++ lib/app/style/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//neutral colors
+Color light = const Color(0xFFececec);
+Color lightHover = const Color(0xFFe2e2e2);
+Color lightActive = const Color(0xFFc3c3c3);
+Color normal = const Color(0xFF3d3d3d);
+Color dark = const Color(0xFF2e2e2e);
+Color darkHover = const Color(0xFF252525);
+Color darkActive = const Color(0xFF1b1b1b);
+Color darker = const Color(0xFF151515);
+
+//accent colors
+Color greenAccent = const Color(0xFF84f269);
+Color yellowAccent = const Color(0xFFEBFF6D);
+Color greenYellow = const Color(0xFFEBFF6D);
+
+//error colors
+Color error = const Color(0xFFC61C1C);
+
+//success colors
+Color success = const Color(0xFF3DC61C);
---
+++ lib/app/style/gradient.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tatarupiah/app/style/colors.dart';
+
+LinearGradient primary = LinearGradient(
+  colors: [
+    greenAccent,
+    yellowAccent,
+  ],
+  begin: Alignment.centerRight,
+  end: Alignment.centerLeft,
+);
