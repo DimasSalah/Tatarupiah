6971aec9760397bdf1a4e29163f324daab96efe3
Dimas Salahuddin <dimassalah10@gmail.com>
2024-02-28 22:08:34 +0700

    feat: Add dropdown functionality to home controller

--- lib/app/modules/home/views/components/dropdown_option.dart
+++ lib/app/modules/home/views/components/dropdown_option.dart
@@ -1,86 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tatarupiah/app/style/text_style.dart';
 
 import '../../../../style/colors.dart';
 
-List menuItems = [
-  "Pendapatan",
-  "Pengeluaran",
-  "Keuntungan",
-];
-
 class DropdownOption extends StatelessWidget {
-  final String label;
-  final Function(dynamic) onItemSelected;
-  const DropdownOption({super.key,
-    required this.label,
-    required this.onItemSelected,});
+  final String dropdownValue;
+  final void Function(String) onItemSelected;
+  const DropdownOption({
+    super.key,
+    required this.dropdownValue,
+    required this.onItemSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return DropdownMenu(
-      width: 200,
-      onSelected: onItemSelected,
-      textStyle: medium.copyWith(color: dark,fontSize: 20),
-      inputDecorationTheme: InputDecorationTheme(
-            isDense: true,
-            filled: true,
-            fillColor: light,
-            contentPadding: const EdgeInsets.fromLTRB(
-              15,
-              0,
-              5,
-              0,
+    return DropdownButtonHideUnderline(
+      child: DropdownButton(
+        iconEnabledColor: light,
+        padding: EdgeInsets.fromLTRB(0, 0, 10, 5),
+        elevation: 0,
+        icon: SvgPicture.asset(
+          'assets/icons/arrow_down.svg',
+        ),
+        iconSize: 31,
+        isDense: true,
+        // iconSize: 30,
+        dropdownColor: darkHover,
+        borderRadius: BorderRadius.circular(14),
+        focusColor: lightHover,
+        style: medium.copyWith(fontSize: 20, color: light),
+        onChanged: (newValue) {
+          onItemSelected(newValue.toString());
+        },
+        value: dropdownValue,
+        items: [
+          "Pemasukan",
+          "Pengeluaran",
+          "Keuntungan",
+        ].map((selectedType) {
+          return DropdownMenuItem(
+            child: new Text(
+              selectedType, style: medium.copyWith(fontSize: 20, color: light)
             ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: const BorderSide(
-                color: Colors.transparent,
-                width: 0,
-              ),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: const BorderSide(
-                color: Colors.transparent,
-                width: 0,
-              ),
-            ),
-          ),
-      menuStyle: MenuStyle(
-            shadowColor: MaterialStateProperty.all(Colors.transparent),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: const BorderRadius.vertical(
-                  bottom: Radius.circular(10),
-                ),
-                side: BorderSide(
-                  color: lightHover,
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-      dropdownMenuEntries: [
-        for (int i = 0; i < menuItems.length; i++)
-          DropdownMenuEntry(
-            value: menuItems[i],
-            label: menuItems[i],
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.transparent),
-              padding: MaterialStateProperty.all(EdgeInsets.zero),
-              textStyle: MaterialStateProperty.all(
-                const TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 20,
-                  fontFamily: "Poppins",
-                  fontWeight: FontWeight.w500,
-                  
-                ),
-              ),
-            ),
-          )
-      ],
+            value: selectedType,
+          );
+        }).toList(),
+      ),
     );
   }
 }
