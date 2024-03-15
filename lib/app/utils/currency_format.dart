0d1fcd174e71fce3db1a7725f396f8e9af242517
Dimas Salahuddin <dimassalah10@gmail.com>
2024-03-15 11:16:47 +0700

    perf: currency format for set rupiah

---
+++ lib/app/utils/currency_format.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class CurrencyInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+
+    final int value = int.parse(newValue.text.replaceAll(',', ''));
+    final String newText = NumberFormat.currency(locale: 'id', decimalDigits: 0, symbol: '').format(value);
+    
+    return newValue.copyWith(
+      text: newText,
+      selection: TextSelection.collapsed(offset: newText.length),
+    );
+  }
+}
+
+String currencyViewFormatter(String value) {
+    if (value.isEmpty) return '0';
+    final formatter = NumberFormat.currency(locale: 'id', decimalDigits: 0, symbol: 'Rp');
+    final amount = int.parse(value);
+    return formatter.format(amount);
+  }
