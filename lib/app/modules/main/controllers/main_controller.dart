20f30406af069abde47d3c09354f5256b513aaad
Dimas Salahuddin <dimassalah10@gmail.com>
2024-03-21 22:53:38 +0700

    refactor: add const in controller main

--- lib/app/modules/main/controllers/main_controller.dart
+++ lib/app/modules/main/controllers/main_controller.dart
@@ -3,15 +3,14 @@ import 'package:tatarupiah/app/modules/chatAi/views/chat_ai_view.dart';
 import 'package:tatarupiah/app/modules/home/views/home_view.dart';
 import 'package:tatarupiah/app/modules/statistics/views/statistics_view.dart';
 
-import '../../../routes/app_pages.dart';
 
 class MainController extends GetxController {
   RxInt selectedIndex = 0.obs;
 
   final screen = [
-    HomeView(),
-    StatisticsView(),
-    ChatAiView()
+    const HomeView(),
+    const StatisticsView(),
+    const ChatAiView()
   ];
 
   void changeIndex(int index) {
