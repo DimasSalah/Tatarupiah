cab0f4160b1a3b77b8191c48011ae9b868d7bf82
dimassalah <dimassalah10@gmail.com>
2024-07-16 01:26:29 +0700

    Refactor main.dart imports and remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:tatarupiah/app/utils/dependency_injection.dart';
-import 'app/utils/global_controller/user_controller.dart';
 import 'app/routes/app_pages.dart';
 import 'app/style/themes.dart';
 
